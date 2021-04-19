5a21410808c4f7f820c6a67a855eb87a81b026ed
Spenser Black <spenserblack01@gmail.com>
2021-04-19 14:27:48 -0400

    Export owner types

--- helpers/default_owner.go
+++ helpers/default_owner.go
@@ -13,8 +13,10 @@ const (
 type OwnerType string
 
 const (
-	org  OwnerType = "org"
-	user OwnerType = "user"
+	// Org signifies that the owner type is a GitHub organization.
+	Org OwnerType = "org"
+	// User signifies that the owner type is a GitHub user.
+	User OwnerType = "user"
 )
 
 // LoadOwner loads the config file for the default owner.
@@ -24,7 +26,7 @@ func LoadOwner() string {
 	if err == errCreatedConfigFile {
 		fmt.Printf("Created owner file in: %v\n", filepath)
 		fmt.Println("Put your owner in this file in the format \"name:type\"")
-		fmt.Printf("Where type is either %q or %q\n", org, user)
+		fmt.Printf("Where type is either %q or %q\n", Org, User)
 	} else if err != nil {
 		log.Fatalf("Unable to load/create owner file: %v", err)
 	}
