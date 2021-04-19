2e979fba2c2fe3409cb0bed6d6b09a208c1ea4d2
Spenser Black <spenserblack01@gmail.com>
2021-04-19 14:02:14 -0400

    Create shared helper for opening config files
    In anticipation of other config options being added.
    
    For #5

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,3 +1,54 @@
 package helpers
 
-const configPath = "/.config/octotui"
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
+
+var configPath = path.Join(".config", "octotui")
+
+var errCreatedConfigFile error = errors.New("File created")
+
+func loadConfigFile(filename string) (contents string, createdPath string, err error) {
+	home, err := os.UserHomeDir()
+
+	if err != nil {
+		err = fmt.Errorf("Unable to get home directory: %w", err)
+		return
+	}
+
+	fullConfigPath := path.Join(home, configPath)
+	fullFilename := path.Join(fullConfigPath, filename)
+
+	if _, innerErr := os.Stat(fullFilename); innerErr != nil {
+		if os.IsNotExist(innerErr) {
+			innerErr := os.Mkdir(fullConfigPath, 0755)
+			if innerErr != nil {
+				err = fmt.Errorf("Unable to create octotui folder in %v", fullConfigPath)
+				return
+			}
+
+			blackListFile, innerErr := os.OpenFile(fullFilename, os.O_RDONLY|os.O_CREATE, 0644)
+			if innerErr != nil {
+				err = fmt.Errorf("Unable to create file %v: %w", fullFilename, innerErr)
+				return
+			}
+			blackListFile.Close()
+
+			createdPath = fullFilename
+			err = errCreatedConfigFile
+			return
+		} else {
+			err = innerErr
+			return
+		}
+	}
+
+	fileContents, err := ioutil.ReadFile(fullFilename)
+	contents = strings.TrimSpace(string(fileContents))
+	return
+}
--- helpers/token.go
+++ helpers/token.go
@@ -2,44 +2,21 @@ package helpers
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
-	"strings"
 )
 
 const (
-	tokenPath = configPath + "/token"
+	tokenFilename = "token"
 )
 
 func LoadToken() string {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal(err)
+	token, filepath, err := loadConfigFile(tokenFilename)
+
+	if err == errCreatedConfigFile {
+		fmt.Printf("Created token file in: %v\n", filepath)
+		fmt.Println("Put your github token in this file")
+	} else if err != nil {
+		log.Fatalf("Unable to load/create token file: %v", err)
 	}
-
-	if _, err := os.Stat(home + tokenPath); err != nil {
-		if os.IsNotExist(err) {
-			err := os.Mkdir(home+"/.config/octotui", 0755)
-			if err != nil {
-				log.Fatal("Unable to create octotui folder in " + home + "/.config")
-			}
-
-			blackListFile, err := os.OpenFile(home+tokenPath, os.O_RDONLY|os.O_CREATE, 0644)
-			if err != nil {
-				log.Fatal("Unable to create token file in " + home + tokenPath)
-			}
-			blackListFile.Close()
-
-			fmt.Println("Created token file in: " + home + tokenPath)
-			fmt.Println("Put your github token in this file")
-		}
-	}
-
-	token, err := ioutil.ReadFile(home + tokenPath)
-	if err != nil {
-		log.Fatal("Can't read token file in: " + home + tokenPath)
-	}
-
-	return strings.TrimSpace(string(token))
+	return token
 }
